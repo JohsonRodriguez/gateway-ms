8c0eccec95e80bf6a95b70d81f155a8596ebc463
@JohsonRodriguez <52184527+JohsonRodriguez@users.noreply.github.com>
2022-01-29 19:19:21 -0500

    Update RouterApplication.java

--- src/main/java/com/lordbyron/router/RouterApplication.java
+++ src/main/java/com/lordbyron/router/RouterApplication.java
@@ -2,6 +2,7 @@ package com.lordbyron.router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableResourceServer
